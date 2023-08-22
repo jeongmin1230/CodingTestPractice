3ef23fa145d92486dcc3d825a08ed70e9175e940
jeongmin <tmxk0810@gmail.com>
2023-08-22 15:02:21 +0900

    - 뒤로 가기 기능 추가 - 문제 골랐을 때 문제 상세 내용, 힌트(클릭 시)내용 출력 기능 추가 - strings 추가
    2023. 8. 22(화)

--- app/src/main/java/com/example/codingtestpractice/MainActivity.kt
+++ app/src/main/java/com/example/codingtestpractice/MainActivity.kt
@@ -6,18 +6,17 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.style.TextAlign
@@ -62,7 +61,7 @@ fun MainScreen() {
         }
         composable("show_detail/{title}", arguments = listOf(navArgument("title") { type = NavType.StringType})) {
             Column(Modifier.fillMaxSize()) {
-                ShowDetail(it.arguments?.getString("title").toString())
+                ShowDetail(it.arguments?.getString("title").toString(), navController)
             }
         }
     }
@@ -70,6 +69,7 @@ fun MainScreen() {
 
 @Composable
 fun SelectProblem(navController: NavHostController) {
+    val context = LocalContext.current
     val problemTitleState = remember { mutableStateOf(emptyList<String>()) }
     val problemTitleRef = FirebaseDatabase.getInstance().getReference("problem")
     val day1Ref = problemTitleRef.child("day 1")
@@ -79,8 +79,7 @@ fun SelectProblem(navController: NavHostController) {
             val day1List = mutableListOf<String>()
             for(childSnapShot in snapshot.children) {
                 val day1Text = childSnapShot.key
-                println(childSnapShot)
-                if(day1Text != null) {
+                if(day1Text != null && !day1Text.contains(" " + context.getString(R.string.hint))) {
                     day1List.add(day1Text.toString())
                 }
             }
@@ -114,7 +113,7 @@ fun ItemLayout(title: String, clickAction: () -> Unit) {
         .fillMaxWidth()
         .clickable { clickAction() }
         .padding(start = 12.dp, bottom = 10.dp, end = 12.dp)
-        .background(Color(0XFFD09AFF), shape = CircleShape),
+        .background(Color(0XFFD09AFF)),
         verticalAlignment = Alignment.CenterVertically
     ) {
         Text(
@@ -131,6 +130,74 @@ fun ItemLayout(title: String, clickAction: () -> Unit) {
 }
 
 @Composable
-fun ShowDetail(title: String) {
-    Text(text = title)
+fun ShowDetail(title: String, navController: NavHostController) {
+    val context = LocalContext.current
+    val problemDetailState = remember { mutableStateOf(emptyList<String>()) }
+    val problemDetailRef = FirebaseDatabase.getInstance().getReference("problem")
+    val detail = remember { mutableStateOf("") }
+    val hintDetail = remember { mutableStateOf("") }
+    val detailRef = problemDetailRef.child("day 1")
+    var showDetail by remember { mutableStateOf(false) }
+
+    val valueEventListener = object : ValueEventListener {
+        override fun onDataChange(snapshot: DataSnapshot) {
+            val problemDetail = mutableListOf<String>()
+            val detailText = snapshot.child(title).value
+            val detailHintText = snapshot.child(title + " " + context.getString(R.string.hint)).value
+            detail.value = detailText.toString()
+            hintDetail.value = detailHintText.toString()
+            problemDetailState.value = problemDetail
+        }
+
+        override fun onCancelled(error: DatabaseError) {
+        }
+    }
+    detailRef.addValueEventListener(valueEventListener)
+    Column {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.padding(vertical = 10.dp)
+        ) {
+            Image(
+                imageVector = ImageVector.vectorResource(id = R.drawable.ic_back),
+                contentDescription = stringResource(id = R.string.ic_back),
+                modifier = Modifier
+                    .clickable(interactionSource = MutableInteractionSource(), indication = null) { navController.popBackStack() }
+                    .padding(horizontal = 10.dp)
+            )
+            Text(text = title)
+        }
+        Column(modifier = Modifier
+            .padding(all = 12.dp)
+            .fillMaxWidth()
+            .background(Color(0XFFD09AFF))) {
+            Text(
+                text = stringResource(id = R.string.question_detail),
+                style = MaterialTheme.typography.bodyMedium.copy(Color.Black),
+                modifier = Modifier.padding(start = 14.dp, top = 12.dp, bottom = 10.dp)
+            )
+            Text(
+                text = detail.value,
+                style = MaterialTheme.typography.bodySmall.copy(Color.Black),
+                modifier = Modifier.padding(start = 12.dp, end = 12.dp, bottom = 12.dp)
+            )
+        }
+        Column(modifier = Modifier
+            .padding(all = 12.dp)
+            .height(100.dp)
+            .fillMaxWidth()
+            .clickable { showDetail = !showDetail }
+            .background(if(showDetail) Color(0XFFD09AFF) else Color(0XFFD09AFF).copy(alpha = 0.5f))) {
+            Text(
+                text = stringResource(id = R.string.hint_detail),
+                style = MaterialTheme.typography.bodyMedium.copy(Color.Black),
+                modifier = Modifier.padding(start = 14.dp, top = 12.dp, bottom = 10.dp)
+            )
+            Text(
+                text = if(showDetail) hintDetail.value else "",
+                style = MaterialTheme.typography.bodySmall.copy(Color.Black),
+                modifier = Modifier.padding(start = 12.dp, end = 12.dp, bottom = 12.dp)
+            )
+        }
+    }
 }
